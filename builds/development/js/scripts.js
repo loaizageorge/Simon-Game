66a8588a25a4b03956dbb79595035bb5429f42fb
George <loaizageorge@gmail.com>
2016-09-08 20:18:04 -0400

    Make boxes flash on click.

--- builds/development/js/scripts.js
+++ builds/development/js/scripts.js
@@ -11,21 +11,25 @@ $(document).ready(function(){
   $("#green").click(function(){
 
       greenSound.play();
+      $('#green').fadeTo(200, 0.5, function() { $('#green').fadeTo(200, 1.0); });
   });
 
   $("#red").click(function(){
 
       redSound.play();
+      $('#red').fadeTo(200, 0.5, function() { $('#red').fadeTo(200, 1.0); });
   });
 
   $("#yellow").click(function(){
 
       yellowSound.play();
+      $('#yellow').fadeTo(200, 0.5, function() { $('#yellow').fadeTo(200, 1.0); });
   });
 
   $("#blue").click(function(){
 
       blueSound.play();
+      $('#blue').fadeTo(200, 0.5, function() { $('#blue').fadeTo(200, 1.0); });
   });
 
 });
