b8eed1f9d17e09db48bc0fd8f18c3011c09e9f0d
George <loaizageorge@gmail.com>
2016-09-08 22:15:48 -0400

    Basic functionality

--- builds/development/js/scripts.js
+++ builds/development/js/scripts.js
@@ -1,23 +1,59 @@
 $(document).ready(function(){
 
+  var simonSequence = [];
+  var mySequence = [];
+  var simonTurn =false;
 
-  var simonSequence = [""];
-  var mySequence = [""];
-  var myTurn =false;
+  var color = "";
 
-  var boxId = "";
-
+  generateSimonPick();
+  console.log(simonSequence);
   $(".box").click(function(){
-    boxId = $(this).attr('id');
-    buttonClicked(boxId);
+    color = $(this).attr('id');
+    buttonClicked(color);
 
   });
 
   function buttonClicked(){
-    var sound = boxId+"Sound";
+    var sound = color+"Sound";
     var playSound = document.getElementById(sound);
+    $("#"+color).fadeTo(200, 0.5, function() { $("#"+color).fadeTo(200, 1.0); });
     playSound.play();
-    $("#"+boxId).fadeTo(200, 0.5, function() { $("#"+boxId).fadeTo(200, 1.0); });
+    buildUpSequence(color);
+    checkSequence();
+
+
+  }
+
+  function buildUpSequence(color){
+    if(simonTurn){
+      simonSequence.push(color);
+    }else{
+      mySequence.push(color);
+    }
+    console.log(mySequence);
+  }
+
+  function checkSequence(){
+    for(var i = 0; i<mySequence.length;i++){
+      if(simonSequence[i]==mySequence[i]){
+        console.log("true");
+      }else{
+        alert("game Over");
+        console.log("false");
+      }
+
+    }
+  }
+
+  function generateSimonPick(){
+    var colorChoices = ["green","red","yellow","blue"];
+    min = Math.ceil(0);
+    max = Math.floor(3);
+    var randomNumber = Math.floor(Math.random() * (max - min + 1)) + min
+    var simonColor = colorChoices[randomNumber];
+    simonSequence.push(simonColor);
+
   }
 
 });
