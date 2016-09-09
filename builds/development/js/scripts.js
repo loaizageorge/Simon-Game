4cc7e17ee17e1fc1c282f89f981709d260392ea0
George <loaizageorge@gmail.com>
2016-09-08 21:20:10 -0400

    Refactor js

--- builds/development/js/scripts.js
+++ builds/development/js/scripts.js
@@ -1,35 +1,23 @@
 $(document).ready(function(){
-  var greenSound = document.getElementById("greenSound");
-  var redSound = document.getElementById("redSound");
-  var yellowSound = document.getElementById("yellowSound");
-  var blueSound = document.getElementById("blueSound");
+
 
   var simonSequence = [""];
   var mySequence = [""];
   var myTurn =false;
 
-  $("#green").click(function(){
+  var boxId = "";
 
-      greenSound.play();
-      $('#green').fadeTo(200, 0.5, function() { $('#green').fadeTo(200, 1.0); });
+  $(".box").click(function(){
+    boxId = $(this).attr('id');
+    buttonClicked(boxId);
+
   });
 
-  $("#red").click(function(){
-
-      redSound.play();
-      $('#red').fadeTo(200, 0.5, function() { $('#red').fadeTo(200, 1.0); });
-  });
-
-  $("#yellow").click(function(){
-
-      yellowSound.play();
-      $('#yellow').fadeTo(200, 0.5, function() { $('#yellow').fadeTo(200, 1.0); });
-  });
-
-  $("#blue").click(function(){
-
-      blueSound.play();
-      $('#blue').fadeTo(200, 0.5, function() { $('#blue').fadeTo(200, 1.0); });
-  });
+  function buttonClicked(){
+    var sound = boxId+"Sound";
+    var playSound = document.getElementById(sound);
+    playSound.play();
+    $("#"+boxId).fadeTo(200, 0.5, function() { $("#"+boxId).fadeTo(200, 1.0); });
+  }
 
 });
