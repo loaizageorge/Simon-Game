a13f86036ae4f7bf13be072cb129b992574bdab2
George <loaizageorge@gmail.com>
2016-09-15 21:46:11 -0400

    Fix bug where player could press buttons during simon's turn

--- builds/development/js/scripts.js
+++ builds/development/js/scripts.js
@@ -1,101 +1,126 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-  var simonSequence = [];
-  var mySequence = [];
-  simonTurnFlag=true;
-  var i = 0;
-  var color = "";
-  var count = "0";
+    var simonSequence = [];
+    var mySequence = [];
+    var simonTurnFlag = true;
+    var dontTouch = true;
 
-  $("#count").html(count);
-  simonPlay();
+    var i = 0;
+    var color = "";
+    var count = "0";
 
-
-
-
-
-
-  $(".box").click(function(){
-    color = $(this).attr('id');
-    simonTurnFlag=false;
-    buttonClicked(color);
-
-
-
-  });
-
-  function buttonClicked(color){
     $("#count").html(count);
-    var sound = color+"Sound";
-    var playSound = document.getElementById(sound);
-    $("#"+color).fadeTo(200, 0.5, function() { $("#"+color).fadeTo(200, 1.0); });
-    playSound.play();
-    if(simonTurnFlag==false){
-      mySequence.push(color);
-      checkSequence();
-    }
-  }
 
-  /*function buildUpSequence(color){
-    if(simonTurnFlag){
-      simonSequence.push(color);
-    }else{
-      mySequence.push(color);
-    }
-    console.log(mySequence);
-  }*/
 
-  function checkSequence(){
-    for(var i = 0; i<mySequence.length;i++){
-      if(simonSequence[i]==mySequence[i]){
-        console.log("Good");
-      }else{
-        alert("gameOver");
+
+    $("#play").click(function() {
+        simonPlay();
+    });
+
+    $("#reset").click(function() {
         gameOver();
+    });
+
+
+
+    $(".box").click(function() {
+        if (simonTurnFlag == false) {
+
+            color = $(this).attr('id');
+            simonTurnFlag = false;
+            buttonClicked(color);
+        }
+
+
+
+    });
+
+    function buttonClicked(color) {
+        $("#count").html(count);
+        var sound = color + "Sound";
+        var playSound = document.getElementById(sound);
+        $("#" + color).fadeTo(200, 0.5, function() {
+            $("#" + color).fadeTo(200, 1.0);
+        });
+        playSound.play();
+        if (simonTurnFlag == false) {
+            mySequence.push(color);
+            checkSequence();
+        }
+    }
+
+    /*function buildUpSequence(color){
+      if(simonTurnFlag){
+        simonSequence.push(color);
+      }else{
+        mySequence.push(color);
       }
+      console.log(mySequence);
+    }*/
 
+    function checkSequence() {
+        for (var i = 0; i < mySequence.length; i++) {
+            if (simonSequence[i] == mySequence[i]) {} else {
+                $("#count").html("!!");
+                setTimeout(function() {
+                    gameOver();
+                }, 1000);
+
+            }
+
+        }
+        if (mySequence.length == simonSequence.length) {
+            simonTurnFlag = true;
+
+            simonPlay();
+        }
     }
-    if(mySequence.length == simonSequence.length){
-      simonTurnFlag=true;
-      simonPlay();
+
+    function generateSimonPick() {
+        var colorChoices = ["green", "red", "yellow", "blue"];
+        min = Math.ceil(0);
+        max = Math.floor(3);
+        var randomNumber = Math.floor(Math.random() * (max - min + 1)) + min
+        var simonColor = colorChoices[randomNumber];
+        simonSequence.push(simonColor);
     }
-  }
 
-  function generateSimonPick(){
-    var colorChoices = ["green","red","yellow","blue"];
-    min = Math.ceil(0);
-    max = Math.floor(3);
-    var randomNumber = Math.floor(Math.random() * (max - min + 1)) + min
-    var simonColor = colorChoices[randomNumber];
-    simonSequence.push(simonColor);
-  }
+    function simonPlay() {
+        count++;
 
-  function simonPlay(){
-    count++;
-
-    i = 0;
-    simonTurnFlag=true;
-    generateSimonPick();
-    playSequence();
-    mySequence = [];
-
-  }
-
-  function playSequence(){
-    setTimeout(function(){
-      buttonClicked(simonSequence[i]);
-      i++;
-
-      if(i<simonSequence.length){
+        i = 0;
+        simonTurnFlag = true;
+        generateSimonPick();
         playSequence();
-      }
-    },1000)
-  }
+        mySequence = [];
 
-  function gameOver(){
-    count=0;
-    simonSequence=[];
-    simonPlay();
-  }
+    }
+
+    function playSequence() {
+        setTimeout(function() {
+            buttonClicked(simonSequence[i]);
+            i++;
+            if(i==simonSequence.length){
+              simonTurnFlag=false;
+            }
+            console.log(i);
+            if (i < simonSequence.length) {
+
+                playSequence();
+
+            }
+        }, 1000);
+    }
+
+    function gameOver() {
+        count = 0;
+        $("#count").html(count);
+        simonSequence = [];
+
+    }
+
+    $("#strict").click(function() {
+        $("#strict-alert").toggleClass("strict-on");
+    });
 
 });
